c32f0821c09ccdc351198809e93c5d0419a97b6b
yiming.qian <yiming_qian@outlook.com>
2023-05-09 20:27:57 +0800

    Update smoothKNNK.m
    bug fix

--- code/OnlinefR/smoothKNNK.m
+++ code/OnlinefR/smoothKNNK.m
@@ -1,10 +1,6 @@
-function r=smoothKNNK(x,N,C,t,focal)
+function pred=smoothKNNK(x,N,C,t,focal)
 Idx = knnsearch(C,x,'K',N);
-pred0=t(Idx,:);
-if focal==0
-    pred=mean(pred0(:,2:3),2);
-else
-    pred=mean(pred0,2);
-end
-r=pred;
+pred0=exp(t(Idx,:));
+pred=mean(pred0,1);
+
 end
